55c86c50746cf302b9a7cd9822ea2003fea81dc7
Artem Nosov <nosov1995@gmail.com>
2023-08-09 16:44:18 +0200

    init user model with auto adding Blog

--- network/users/admin.py
+++ network/users/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import CustomUser
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'password', 'is_superuser')
+    empty_value_display = '-пусто-'
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.set_password(form.cleaned_data['password'])
+            super().save_model(request, obj, form, change)
+            from blogs.models import Blog
+            if not Blog.objects.filter(author=obj).exists():
+                Blog.objects.create(author=obj)
+        else:
+            super().save_model(request, obj, form, change)
--- network/users/models.py
+++ network/users/models.py
@@ -1,3 +1,27 @@
-from django.db import models
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import AbstractUser, UserManager
 
-# Create your models here.
+
+class CustomUserManager(UserManager):
+
+    def create_user(self, username, email=None, password=None, **extra_fields):
+        user = super().create_user(username, email, password, **extra_fields)
+        from blogs.models import Blog
+        if not Blog.objects.filter(author=user).exists():
+            Blog.objects.create(author=user)
+        return user
+
+
+class CustomUser(AbstractUser):
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.password = make_password(self.password)
+            super().save(*args, **kwargs)
+            from blogs.models import Blog
+            Blog.objects.create(author=self)
+        else:
+            super().save(*args, **kwargs)
