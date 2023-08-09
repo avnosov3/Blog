5c6c4838dc1945a355dcb6ae8ad6b3a94f335c95
Artem Nosov <nosov1995@gmail.com>
2023-08-09 18:08:20 +0200

    add follow to admin

--- network/blogs/admin.py
+++ network/blogs/admin.py
@@ -1,16 +1,23 @@
 from django.contrib import admin
 
-from .models import Blog, Post
+from . import models
 
 
-@admin.register(Blog)
+@admin.register(models.Blog)
 class BlogAdmin(admin.ModelAdmin):
     list_display = ('pk', 'author')
     empty_value_display = '-пусто-'
 
 
-@admin.register(Post)
+@admin.register(models.Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('pk', 'title', 'pub_date', 'blog')
     list_filter = ('pub_date', 'blog')
+    empty_value_display = '-пусто-'
+
+
+@admin.register(models.Follow)
+class FollowAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'user', 'blog')
+    list_filter = ('user', 'blog')
     empty_value_display = '-пусто-'
