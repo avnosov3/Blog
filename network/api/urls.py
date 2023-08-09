df5bc8adf7d327de28865128ba04175b268caad4
Artem Nosov <nosov1995@gmail.com>
2023-08-09 20:25:12 +0200

    init feed

--- network/api/urls.py
+++ network/api/urls.py
@@ -6,6 +6,9 @@ from . import views
 router_v1 = routers.DefaultRouter()
 router_v1.register(r'posts', views.PostViewSet, 'post')
 router_v1.register(r'follow', views.FollowViewSet, basename='follow')
+router_v1.register(
+    r'feed/(?P<user_id>\d+)', views.FeedViewSet, basename='feed'
+)
 
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
--- network/api/views.py
+++ network/api/views.py
@@ -1,5 +1,8 @@
 from blogs.models import Follow, Post
+from django.db.models import F
+from django.shortcuts import get_list_or_404, get_object_or_404
 from rest_framework import filters, mixins, permissions, viewsets
+from users.models import CustomUser
 
 from . import serializers
 
@@ -18,3 +21,14 @@ class FollowViewSet(
 ):
     queryset = Follow.objects.all()
     serializer_class = serializers.FollowSerializer
+
+
+class FeedViewSet(
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet
+):
+    serializer_class = serializers.PostSerializer
+
+    def get_queryset(self):
+        user = get_object_or_404(CustomUser, pk=self.kwargs.get('user_id'))
+        return Post.objects.filter(blog__followers__user=user)
