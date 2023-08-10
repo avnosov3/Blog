b96ecaeeaa17834f985559d10524f8a15b459a06
Artem Nosov <nosov1995@gmail.com>
2023-08-11 00:30:13 +0200

    change cache time

--- network/api/constants.py
+++ network/api/constants.py
@@ -3,4 +3,4 @@ SUBSCRIBE_SELF = 'Вы не можете подписаться сами на с
 FEED_LIMITS = 500
 READ_STATUS_EXISTS = 'Вы уже отметили, что прочитали этот пост'
 FEED_CACHE_PATH = 'user_feed_{}'
-CACHE_TIME = 60 * 2
+CACHE_TIME = 60 * 60
